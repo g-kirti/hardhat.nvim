744e60e3f934fa694b3a0db896d08856051f28d6
Keerthi Ganesh <gkeerthi1999@gmail.com>
2026-05-22 18:33:26 +0800

    style(palette): colour changes

--- lua/hardhat/palettes/m1.lua
+++ lua/hardhat/palettes/m1.lua
@@ -14,29 +14,29 @@ M.load_colors = function()
     black         = "#1a1d14",
     black_light   = "#707761",
 
-    red           = "#c8413e",
-    red_light     = "#ea827f",
-    red_dim       = "#9f3538",
+    red           = "#c8653e",
+    red_light     = "#eb9d7f",
+    red_dim       = "#9e5234",
 
     green         = "#a5b218",
     green_light   = "#d0df6c",
     green_dim     = "#8f9b2f",
 
-    yellow        = "#d4bc22",
+    yellow        = "#ccb521",
     yellow_light  = "#eee58a",
     yellow_dim    = "#b69e0a",
 
-    blue          = "#2f9992",
-    blue_light    = "#74b7b2",
-    blue_dim      = "#1f716d",
+    blue          = "#2f9982",
+    blue_light    = "#74b8a9",
+    blue_dim      = "#1e705e",
 
-    orange        = "#c28257",
-    orange_light  = "#e0b38b",
-    orange_dim    = "#91603f",
+    orange        = "#c2a259",
+    orange_light  = "#d9c086",
+    orange_dim    = "#a88d4d",
 
-    cyan          = "#3f7ca9",
-    cyan_light    = "#8cb6d4",
-    cyan_dim      = "#306185",
+    cyan          = "#3f90a9",
+    cyan_light    = "#8cc3d4",
+    cyan_dim      = "#337a8f",
 
     white         = "#acac9c",
     white_light   = "#c2c1b4",
